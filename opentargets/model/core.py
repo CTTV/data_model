a35f886361ab84d24bd11a949d1b3a637d0f2568
Gautier Koscielny <gkos.bio@gmail.com>
2018-08-13 15:34:44 +0100

    Add license in preamble

--- opentargets/model/core.py
+++ opentargets/model/core.py
@@ -1,6 +1,8 @@
 '''
 Copyright 2014-2017 EMBL - European Bioinformatics Institute, Wellcome
 Trust Sanger Institute, GlaxoSmithKline and Biogen
+Copyright 2018 Biogen, Celgene Corporation, EMBL - European Bioinformatics Institute, GlaxoSmithKline,
+Takeda Pharmaceutical Company and Wellcome Sanger Institute
 
 This software was developed as part of Open Targets. For more information please see:
 
@@ -33,7 +35,8 @@ import opentargets.model.evidence.drug as evidence_drug
 import opentargets.model.evidence.genetics as evidence_genetics
 
 __author__ = "Gautier Koscielny"
-__copyright__ = "Copyright 2014-2018, Open Targets"
+__copyright__ = "Copyright 2014-2017 EMBL - European Bioinformatics Institute, Wellcome Trust Sanger Institute, GlaxoSmithKline and Biogen"
+__copyright__ = "Copyright 2018 Biogen, Celgene Corporation, EMBL - European Bioinformatics Institute, GlaxoSmithKline, Takeda Pharmaceutical Company and Wellcome Sanger Institute"
 __credits__ = ["Gautier Koscielny", "Samiul Hasan"]
 __license__ = "Apache 2.0"
 __version__ = "1.2.8"
